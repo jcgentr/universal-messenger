f0c0f53d88a596b908aadc6b6f5a069f19c2061a
Jared Gentry <jcgentr@clemson.edu>
2022-07-31 15:01:51 -0500

    Commit what I already have locally

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const port = 3000;
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    console.log("total clients connected: ", io.sockets.sockets.size);
+    socket.broadcast.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}/`);
+});
